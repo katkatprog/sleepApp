3c7b18ca20f753c55dfd8591b35fc783c248e580
katsu-crypto <kokatsuplayer@gmail.com>
2024-02-18 23:11:55 +0900

    Homeページ 修正

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,8 +1,14 @@
-export default function Home() {
+const Home = () => {
   const numsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  return numsArray.map((num) => (
-    <h1 key={num} className=" h-20">
-      {num}
-    </h1>
-  ));
-}
+  return (
+    <>
+      {numsArray.map((num) => (
+        <h1 key={num} className=" h-20">
+          {num}
+        </h1>
+      ))}
+    </>
+  );
+};
+
+export default Home;
