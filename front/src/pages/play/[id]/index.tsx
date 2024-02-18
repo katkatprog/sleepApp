d4a9cf44d2c1e1176ae3f884c955d03872e05e3b
katsu-crypto <kokatsuplayer@gmail.com>
2024-02-18 18:47:02 +0900

    音声再生ページ　中央にアイコン(仮)を配置

--- front/src/pages/play/[id]/index.tsx
+++ front/src/pages/play/[id]/index.tsx
@@ -37,7 +37,22 @@ const Play = () => {
         </div>
       </div>
 
-      <div className="mt-10 flex justify-center">
+      <div className="flex justify-center mt-8">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-40 h-40 text-emerald-400"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+
+      <div className="h-20 mt-10 flex justify-center">
         <audio controls src={soundInfo.url}></audio>
       </div>
     </div>
