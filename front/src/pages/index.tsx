8daff1881cade888160d67c23376df342dd11b3d
katsu-crypto <kokatsuplayer@gmail.com>
2024-02-19 20:40:11 +0900

    トップページに音声再生ページへのリンクを設置(いずれ削除予定)

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 const HomePage = () => {
   const numsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   return (
     <>
+      <Link href={"/play/1"}>音声再生ページ</Link>
       {numsArray.map((num) => (
         <h1 key={num} className=" h-20">
           {num}
