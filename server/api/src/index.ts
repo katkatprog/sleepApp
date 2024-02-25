3bc5f4e924e46a1d313079df8a52a2d2d19d23c0
katsu-crypto <kokatsuplayer@gmail.com>
2024-02-25 13:22:47 +0900

    express導入

--- server/api/src/index.ts
+++ server/api/src/index.ts
@@ -1,3 +1,7 @@
 import "dotenv/config";
+import express from "express";
 
-console.log("Hello from API");
+const app = express();
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server started at ${process.env.PORT} port!`);
+});
