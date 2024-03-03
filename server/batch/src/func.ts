d55217a5a95b2a27961f6fdee98e3f613d25360e
katsu-crypto <kokatsuplayer@gmail.com>
2024-03-04 01:00:12 +0900

    GPTへの依頼のスクリプトを英語に変更

--- server/batch/src/func.ts
+++ server/batch/src/func.ts
@@ -17,13 +17,9 @@ export const generateDailyWordsList = async () => {
   const chatCompletion = await openai.chat.completions.create({
     messages: [
       {
-        role: "system",
-        content: "日本語で返答してください。",
-      },
-      {
         role: "user",
         content:
-          "100個の名詞をJSONの配列形式で表示してください。※ただし重複させてはいけません。※改行なしで表示してください。",
+          "Please pickup 200 random nouns.\n※Show them in Japanese\n※Show them using array format(JSON) with no indentation.\n※Avoid duplicates as far as possible.",
       },
     ],
     model: "gpt-3.5-turbo",
