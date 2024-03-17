6197c3ec9338c85294398fff26eb55be942d459f
katsu-crypto <kokatsuplayer@gmail.com>
2024-03-17 22:41:46 +0900

    テーマ画像、アイコンの作成

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import React from "react";
-import { MoonIcon } from "./icons/MoonIcon";
+import Image from "next/image";
 
 export const Header = () => {
   return (
     <header className="h-16 bg-neutral-800 border-gray-300 border-b flex items-center justify-center fixed w-full left-0 top-0">
       <Link href={"/"}>
-        <MoonIcon propClassName="w-14 h-14 p-1 text-emerald-400 hover:bg-neutral-700 rounded-full transition"></MoonIcon>
+        <Image
+          src={"/prehnite_icon.svg"}
+          alt="#"
+          width={"48"}
+          height={"48"}
+        ></Image>
       </Link>
     </header>
   );
