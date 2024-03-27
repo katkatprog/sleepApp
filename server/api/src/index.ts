a0b32a61ed8c097c7c4cd04a6504580c84cd9326
katsu-crypto <kokatsuplayer@gmail.com>
2024-03-27 17:27:07 +0000

    自動デプロイできているかどうかの確認のため、ヘルスメッセージ修正

--- server/api/src/index.ts
+++ server/api/src/index.ts
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 
 app.use("/sound-info", soundInfoRouter);
+app.get("/api/health", async (req, res)=>{
+  return res.json({
+    status: "OK",
+    message: "update at 20240328"
+  })
+})
 
 app.listen(8080, () => {
   console.log(`API Server started...`);
