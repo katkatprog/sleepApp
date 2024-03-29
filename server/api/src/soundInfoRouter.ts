057a1b9bca4a851016886ad6e97d097a7408fe07
katsu-crypto <kokatsuplayer@gmail.com>
2024-03-29 12:22:36 +0000

    音声検索ページの総数を求める機能を作成

--- server/api/src/soundInfoRouter.ts
+++ server/api/src/soundInfoRouter.ts
@@ -2,33 +2,33 @@ import { PrismaClient } from "@prisma/client";
 import express from "express";
 const prisma = new PrismaClient();
 export const soundInfoRouter = express.Router();
+const numSoundsPerPage = 20;
 
-// 音声情報取得(個別)
-soundInfoRouter.get("/:id", async (req, res) => {
-  const numParam = Number(req.params.id);
-  if (isNaN(numParam)) {
-    return res.status(400).send("Request Param is not valid...");
-  }
-  try {
-    const result = await prisma.soundInfo.findUnique({
-      where: { id: numParam },
-    });
-    if (!result) {
-      return res.status(404).send("Sound info was not found...");
-    }
-    return res.send(result);
-  } catch (error) {
-    console.log("エラー発生");
-    console.log(error);
-    return res.status(500).send("Something went wrong...");
-  }
-});
+// // 音声情報取得(個別)
+// soundInfoRouter.get("/:id", async (req, res) => {
+//   const numParam = Number(req.params.id);
+//   if (isNaN(numParam)) {
+//     return res.status(400).send("Request Param is not valid...");
+//   }
+//   try {
+//     const result = await prisma.soundInfo.findUnique({
+//       where: { id: numParam },
+//     });
+//     if (!result) {
+//       return res.status(404).send("Sound info was not found...");
+//     }
+//     return res.send(result);
+//   } catch (error) {
+//     console.log("エラー発生");
+//     console.log(error);
+//     return res.status(500).send("Something went wrong...");
+//   }
+// });
 
 // 音声情報取得(一覧)
 soundInfoRouter.get("/list/:page", async (req, res) => {
-  const numSoundsPerPage = 20;
-  const numPage = Number(req.params.page);
-  if (isNaN(numPage)) {
+  const numCurrentPage = Number(req.params.page);
+  if (isNaN(numCurrentPage)) {
     return res.status(400).send("Request Param is not valid...");
   }
 
@@ -39,9 +39,22 @@ soundInfoRouter.get("/list/:page", async (req, res) => {
         createdAt: "desc",
       },
       take: numSoundsPerPage,
-      skip: numSoundsPerPage * (numPage - 1),
+      skip: numSoundsPerPage * (numCurrentPage - 1),
     });
     return res.send(result);
+  } catch (error) {
+    console.log("エラー発生");
+    console.log(error);
+    return res.status(500).send("Something went wrong...");
+  }
+});
+
+// 音声検索ページの総数を計算
+soundInfoRouter.get("/num-of-explore-pages", async (req, res) => {
+  try {
+    const numSounds = await prisma.soundInfo.count();
+    const numPages = Math.ceil(numSounds / numSoundsPerPage);
+    return res.send({ numPages });
   } catch (error) {
     console.log("エラー発生");
     console.log(error);
