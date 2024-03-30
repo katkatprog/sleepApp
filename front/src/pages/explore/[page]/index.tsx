8e28ea1a8131ade378e7ecb50058ee0929d30ccb
katsu-crypto <kokatsuplayer@gmail.com>
2024-03-30 16:53:08 +0900

    無駄なpropsを排除

--- front/src/pages/explore/[page]/index.tsx
+++ front/src/pages/explore/[page]/index.tsx
@@ -4,8 +4,12 @@ import { SoundInfo } from "@prisma/client";
 import { GetServerSideProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const ExplorePage = (props: SoundsListProps) => {
+  const router = useRouter();
+  const currentPage = Number(router.query.page);
+
   return (
     <>
       {props.soundsList.map((sound) => (
@@ -36,16 +40,16 @@ const ExplorePage = (props: SoundsListProps) => {
       ))}
       <div className="h-28 pt-4 flex items-start justify-center">
         <div className="flex items-center">
-          {props.currentPage > 1 && (
-            <Link href={`/explore/${props.currentPage - 1}`}>
+          {currentPage > 1 && (
+            <Link href={`/explore/${currentPage - 1}`}>
               <button className="rounded-md px-2 py-2 border border-neutral-700 hover:bg-neutral-700 transition mr-4">
                 <ArrowLongLeftIcon propClassName=""></ArrowLongLeftIcon>
               </button>
             </Link>
           )}
-          {`${props.currentPage} / ${props.totalPages}`}
-          {props.currentPage < props.totalPages && (
-            <Link href={`/explore/${props.currentPage + 1}`}>
+          {`${currentPage} / ${props.totalPages}`}
+          {currentPage < props.totalPages && (
+            <Link href={`/explore/${currentPage + 1}`}>
               <button className="rounded-md px-2 py-2 border border-neutral-700 hover:bg-neutral-700 transition ml-4">
                 <ArrowLongRightIcon propClassName=""></ArrowLongRightIcon>
               </button>
@@ -73,7 +77,6 @@ export const getServerSideProps: GetServerSideProps<SoundsListProps> = async (
   return {
     props: {
       soundsList,
-      currentPage: Number(context.params?.page),
       totalPages,
     },
   };
@@ -81,6 +84,5 @@ export const getServerSideProps: GetServerSideProps<SoundsListProps> = async (
 
 interface SoundsListProps {
   soundsList: SoundInfo[];
-  currentPage: number;
   totalPages: number;
 }
