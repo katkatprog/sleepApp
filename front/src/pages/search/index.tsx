f2a464a40535a586c27140bab46857021fd08736
katsu-crypto <kokatsuplayer@gmail.com>
2024-03-30 09:50:23 +0000

    音声検索URL変更（Front側）

--- front/src/pages/search/index.tsx
+++ front/src/pages/search/index.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const ExplorePage = (props: SoundsListProps) => {
+const SearchPage = (props: SoundsListProps) => {
   const router = useRouter();
   const currentPage = Number(router.query.page);
 
@@ -41,7 +41,7 @@ const ExplorePage = (props: SoundsListProps) => {
       <div className="h-28 pt-4 flex items-start justify-center">
         <div className="flex items-center">
           {currentPage > 1 && (
-            <Link href={`/explore?page=${currentPage - 1}`}>
+            <Link href={`/search?page=${currentPage - 1}`}>
               <button className="rounded-md px-2 py-2 border border-neutral-700 hover:bg-neutral-700 transition mr-4">
                 <ArrowLongLeftIcon propClassName=""></ArrowLongLeftIcon>
               </button>
@@ -49,7 +49,7 @@ const ExplorePage = (props: SoundsListProps) => {
           )}
           {`${currentPage} / ${props.totalPages}`}
           {currentPage < props.totalPages && (
-            <Link href={`/explore?page=${currentPage + 1}`}>
+            <Link href={`/search?page=${currentPage + 1}`}>
               <button className="rounded-md px-2 py-2 border border-neutral-700 hover:bg-neutral-700 transition ml-4">
                 <ArrowLongRightIcon propClassName=""></ArrowLongRightIcon>
               </button>
@@ -61,14 +61,16 @@ const ExplorePage = (props: SoundsListProps) => {
   );
 };
 
-export default ExplorePage;
+export default SearchPage;
 
 export const getServerSideProps: GetServerSideProps<SoundsListProps> = async (
   context,
 ) => {
   // APIから音声リストを取得
   const response = await Promise.all([
-    fetch(`${process.env.API_URL}/sound-info/search?page=${context.query?.page}`),
+    fetch(
+      `${process.env.API_URL}/sound-info/search?page=${context.query?.page}`,
+    ),
     fetch(`${process.env.API_URL}/sound-info/total-search-result-pages`),
   ]);
   const soundsList: SoundInfo[] = await response[0].json();
