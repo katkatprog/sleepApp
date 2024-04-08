39d1b722af835fe298c1d812ba6fabfa0bd1c94a
katsu-crypto <kokatsuplayer@gmail.com>
2024-04-08 16:05:08 +0000

    テストコード簡略化

--- batch/src/func.test.ts
+++ batch/src/func.test.ts
@@ -27,14 +27,8 @@ describe("SSML作成処理のテスト", () => {
 });
 
 describe("配列シャッフル処理のテスト", () => {
-  let inputArray: string[];
-  let outputArray: string[];
-
-  beforeAll(() => {
-    inputArray = ["a", "b", "c", "d", "e"];
-    outputArray = arrayShuffle(inputArray);
-    console.log(outputArray);
-  });
+  const inputArray = ["a", "b", "c", "d", "e"];
+  const outputArray = arrayShuffle(inputArray);
 
   it("シャッフル前後で配列の長さが同じことのテスト", () => {
     expect(inputArray.length).toBe(outputArray.length);
@@ -45,9 +39,5 @@ describe("配列シャッフル処理のテスト", () => {
       const existEle = outputArray.find((output) => output === input); //要素が存在すればその要素、存在しなければundefinedが返ってくる
       expect(existEle).not.toBe(undefined);
     });
-  });
-
-  it("シャッフル前後でinput配列の変更がないことのテスト", () => {
-    expect(inputArray).toEqual(inputArray);
   });
 });
