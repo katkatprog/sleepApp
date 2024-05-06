424f180c8a9fbdd8e3e20f9c4f6286678b5e8f9e
katkatprog <katkatprog@gmail.com>
2024-05-06 10:37:20 +0000

    自動テストのため、appオブジェクトを切り出し

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,4 @@
-import "dotenv/config";
-import express from "express";
-import { soundInfoRouter } from "./soundInfoRouter";
-
-const app = express();
-app.use(express.json());
-
-app.use("/sound-info", soundInfoRouter);
-app.get("/health", async (req, res)=>{
-  return res.json({
-    status: "OK",
-    message: "update at 20240331"
-  })
-})
+import { app } from "./app";
 
 app.listen(8080, () => {
   console.log(`API Server started...`);
