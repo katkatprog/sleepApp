d89514051e31ae6bd29ae2f70d3cbaf777f03f16
katkatprog <katkatprog@gmail.com>
2024-05-06 12:27:46 +0000

    自動テスト成功（サンプル）

---
+++ api/src/test/integration.test.ts
@@ -0,0 +1,23 @@
+import { SoundInfo } from "@prisma/client"
+import { prismaMock } from "../singleton"
+import request from "supertest"
+import { app } from "../app"
+
+test('should create new user ', async () => {
+  const sInfo:SoundInfo = {
+    id: 1,
+    name:"test",
+    createdAt: (new Date(0)).toString() as unknown as Date,
+    url:"sampleurl",
+    isMaleVoice: true,
+    playCount:0
+  }
+
+  prismaMock.soundInfo.findUnique.mockResolvedValue(sInfo)
+  prismaMock.soundInfo.update.mockResolvedValue({...sInfo, playCount: 1})
+
+  const res = await request(app).get("/sound-info/single/1")
+  expect(res.status).toBe(200)
+  expect(res.body).toEqual({...sInfo, playCount: 1})
+})
+
