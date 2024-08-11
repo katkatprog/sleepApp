4030d913229e62bc18da73e811299afdcc9b31ef
katkatprog <katkatprog@gmail.com>
2024-08-11 09:07:49 +0000

    フロント側 signin機能作成

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,15 +2,22 @@ import "dotenv/config";
 import express from "express";
 import { soundInfoRouter } from "./soundInfoRouter";
 import { authRouter } from "./authRouter";
+import cors from "cors";
 
 export const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 app.use("/sound-info", soundInfoRouter);
 app.use("/auth", authRouter);
-app.get("/health", async (req, res)=>{
+app.get("/health", async (req, res) => {
   return res.json({
     status: "OK",
-    message: "update at 20240331"
-  })
-})
+    message: "update at 20240331",
+  });
+});
--- front/src/pages/signin/index.tsx
+++ front/src/pages/signin/index.tsx
@@ -1,16 +1,70 @@
 import { Layout } from "@/components/Layout";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useRef } from "react";
 
-const AuthPage = () => {
+const SigninPage = () => {
+  const router = useRouter();
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
   return (
     <Layout>
       <div className="px-8 pt-10">
         <div className="flex justify-center">
-          <h1>Signin to Prehnite</h1>
+          <div className="flex-col max-w-xs w-full">
+            <h1 className="text-2xl font-black">Prehniteにログイン</h1>
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                try {
+                  const result = await fetch(
+                    `${process.env.NEXT_PUBLIC_API_URL}/auth/signin`,
+                    {
+                      method: "POST",
+                      body: JSON.stringify({
+                        email: emailRef.current?.value,
+                        password: passwordRef.current?.value,
+                      }),
+                      headers: {
+                        "content-type": "application/json",
+                      },
+                      credentials: "include",
+                    },
+                  );
+                  if (result.status === 200) {
+                    router.push(`/search`);
+                  } else {
+                    alert("ログインに失敗しました");
+                  }
+                } catch (error) {
+                  console.log(error);
+                  alert("ログインに失敗しました");
+                }
+              }}
+            >
+              <input
+                ref={emailRef}
+                type="text"
+                className="h-10 bg-neutral-800 border-2 border-neutral-700 placeholder:text-gray-600 rounded-lg pl-2 mt-4 outline-neutral-500 w-full"
+                placeholder="Input your email..."
+              />
+
+              <input
+                ref={passwordRef}
+                type="password"
+                className="h-10 bg-neutral-800 border-2 border-neutral-700 placeholder:text-gray-600 rounded-lg pl-2 mt-4 outline-neutral-500 w-full"
+                placeholder="Input your password..."
+              />
+
+              <button className="mt-6 bg-green-600 hover:bg-green-500 font-bold w-full py-2 rounded-md transition">
+                Signin
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </Layout>
   );
 };
 
-export default AuthPage;
+export default SigninPage;
