b0d79feadcc22c059a5908a63923ee3078ad25a0
katkatprog <katkatprog@gmail.com>
2024-08-13 14:34:49 +0000

    Front側 ログアウト用APIを呼ぶ処理を作成

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -5,8 +5,11 @@ import { SettingIcon } from "./icons/SettingIcon";
 import { LoginIcon } from "./icons/LoginIcon";
 import { LoginUserContext } from "@/pages/_app";
 import { LogoutIcon } from "./icons/LogoutIcon";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 export const Footer = () => {
+  const router = useRouter();
   const loginUser = useContext(LoginUserContext);
 
   return (
@@ -18,7 +21,32 @@ export const Footer = () => {
         <SettingIcon propClassName="w-9 h-9 p-1 rounded-full stroke-2 hover:bg-neutral-700 transition"></SettingIcon>
         {loginUser ? (
           // ログイン状態
-          <LogoutIcon propClassName="w-9 h-9 p-1 rounded-full stroke-2 hover:bg-neutral-700 transition"></LogoutIcon>
+          <button
+            onClick={async () => {
+              try {
+                const result = await fetch(
+                  `${process.env.NEXT_PUBLIC_API_URL}/auth/signout/`,
+                  {
+                    method: "POST",
+                    headers: {
+                      "content-type": "application/json",
+                    },
+                    credentials: "include",
+                  },
+                );
+                if (result.status === 200) {
+                  router.push("/");
+                  toast.success("ログアウトしました。", { autoClose: 5000 });
+                } else {
+                  toast.error("ログアウトに失敗しました。再度お試しください。");
+                }
+              } catch (error) {
+                toast.error("ログアウトに失敗しました。再度お試しください。");
+              }
+            }}
+          >
+            <LogoutIcon propClassName="w-9 h-9 p-1 rounded-full stroke-2 hover:bg-neutral-700 transition"></LogoutIcon>
+          </button>
         ) : (
           // 未ログイン状態
           <Link href={"/signin"}>
