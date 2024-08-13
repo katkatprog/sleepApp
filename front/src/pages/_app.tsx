bd23eabc4f61c17a097231d73bdb46d3afdc0d15
katkatprog <katkatprog@gmail.com>
2024-08-13 09:16:52 +0000

    画面ロード時、ログインユーザー情報取得するように修正

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -2,8 +2,27 @@ import { Layout } from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
+import { User } from "@prisma/client";
+import { createContext, useEffect, useState } from "react";
+
+export const LoginUserContext = createContext<LoginUser | null>(null);
 
 export default function App({ Component, pageProps }: AppProps) {
+  // 画面ロード時にログインユーザーを取得し、設定する
+  const [loginUser, setLoginUser] = useState<LoginUser | null>(null);
+  useEffect(() => {
+    // useEffect内でasync関数を実行するため、即時実行の形にしている
+    (async () => {
+      const result = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/login-user/`,
+        {
+          credentials: "include",
+        },
+      );
+      setLoginUser(await result.json());
+    })();
+  }, []);
+
   return (
     <>
       {/* Toastを全ページ、ページ遷移時も表示できるように当ファイルに記載 */}
@@ -12,7 +31,12 @@ export default function App({ Component, pageProps }: AppProps) {
         theme={"colored"}
         style={{ width: "500px" }}
       />
-      <Component {...pageProps} />
+      {/* Layoutで囲われている全ページでLoginUserをグローバル的に使えるように設定 */}
+      <LoginUserContext.Provider value={loginUser}>
+        <Component {...pageProps} />
+      </LoginUserContext.Provider>
     </>
   );
 }
+
+type LoginUser = Omit<User, "hashedPassword">;
