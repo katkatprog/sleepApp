1190c4504229bd1d873eb7da604e96e460dcab3f
katkatprog <katkatprog@gmail.com>
2024-08-17 06:41:12 +0000

    音声のいいね付け外しAPI パフォーマンスを考慮したロジックに変更

--- api/src/router/soundFavoriteRouter.ts
+++ api/src/router/soundFavoriteRouter.ts
@@ -3,6 +3,7 @@ import prisma from "../prisma/client";
 import { param } from "express-validator";
 import { checkReq } from "../middleware/checkReq";
 import { checkJwt } from "../middleware/checkJwt";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export const soundFavoriteRouter = express.Router();
 
@@ -42,18 +43,22 @@ soundFavoriteRouter.post(
   checkReq,
   checkJwt,
   async (req, res) => {
+    // いいねの状態に関わらず、まずはいいねを付けに行く（レコードを作成しに行く）
+    // ・レコード作成成功したら、いいね付け成功
+    // ・重複エラーが起きた場合は、既にいいねが付いているということであるため、いいねを外す（レコードを削除する）
     try {
-      const result = await prisma.soundFavorite.findUnique({
-        where: {
-          userId_soundId: {
-            userId: res.locals.userId as number,
-            soundId: parseInt(req.params.soundId as string),
-          },
+      // レコード作成（いいねをつける）
+      await prisma.soundFavorite.create({
+        data: {
+          userId: res.locals.userId as number,
+          soundId: parseInt(req.params.soundId as string),
         },
       });
 
-      if (result) {
-        // レコードがある（いいねがついている）→ レコード削除（いいねを外す）
+      return res.status(200).send("OK");
+    } catch (error) {
+      if ((error as PrismaClientKnownRequestError).code === "P2002") {
+        // 重複エラーの場合は既にレコードがある（いいねがついている）→ レコード削除（いいねを外す）
         await prisma.soundFavorite.delete({
           where: {
             userId_soundId: {
@@ -62,18 +67,8 @@ soundFavoriteRouter.post(
             },
           },
         });
-      } else {
-        // レコードが無い（いいねがついてない）→ レコード作成（いいねをつける）
-        await prisma.soundFavorite.create({
-          data: {
-            userId: res.locals.userId as number,
-            soundId: parseInt(req.params.soundId as string),
-          },
-        });
+        return res.status(200).send("OK");
       }
-
-      return res.status(200).send("OK");
-    } catch (error) {
       return res.status(500).send("想定外のエラーが発生しました。");
     }
   },
