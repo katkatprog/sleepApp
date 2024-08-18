ad8b9c757432aae9d2e9e7a8e83cc8fc3d86295f
katkatprog <katkatprog@gmail.com>
2024-08-18 06:48:29 +0000

    いいね一覧、音声リクエストページにおいて、データの取得が終わるまでローディングアイコンを表示するようにした

--- front/src/pages/favorite/index.tsx
+++ front/src/pages/favorite/index.tsx
@@ -9,6 +9,7 @@ import { ArrowLongLeftIcon } from "@/components/icons/ArrowLongLeftIcon";
 import { ArrowLongRightIcon } from "@/components/icons/ArrowLongRightIcon";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { Loading } from "@/components/Loading";
 
 const FavoritePage = () => {
   const router = useRouter();
@@ -16,6 +17,7 @@ const FavoritePage = () => {
   const currentPage = Number(router.query.page || 1);
   const [soundsList, setSoundsList] = useState<SoundInfo[]>([]);
   const [totalPages, setTotalPages] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     // async, awaitを使うため、即時実行関数の形にする
@@ -32,6 +34,7 @@ const FavoritePage = () => {
         const resultJson = await result.json();
         setSoundsList(resultJson.soundsList);
         setTotalPages(resultJson.totalPages);
+        setIsLoading(false);
       }
     })();
 
@@ -41,70 +44,76 @@ const FavoritePage = () => {
   }, [context.loginUser, currentPage]);
 
   return (
-    <Layout>
-      <Head>
-        <title>いいねした音声 / Prehnite</title>
-      </Head>
-      <h1 className="text-2xl font-black m-6">いいねした音声</h1>
-      {soundsList.length === 0 ? (
-        <h1 className="text-center">いいねした音声がありません</h1>
+    <>
+      {isLoading ? (
+        <Loading propClassName="h-80 flex justify-center items-center"></Loading>
       ) : (
-        soundsList.map((sound) => (
-          <Link href={`/play/${sound.id}`} key={sound.id}>
-            <div className="h-20 px-4 pt-2 border-b flex justify-between items-start border-neutral-700 hover:bg-neutral-700 transition">
-              <div className="flex pt-2">
-                <h1 className="font-bold mr-2">{sound.name}</h1>
-                {sound.isMaleVoice !== null &&
-                  (sound.isMaleVoice ? (
-                    <Image
-                      alt="#"
-                      src={"/etc/male_icon.svg"}
-                      width={32}
-                      height={32}
-                    ></Image>
-                  ) : (
-                    <Image
-                      alt="#"
-                      src={"/etc/female_icon.svg"}
-                      width={32}
-                      height={32}
-                    ></Image>
-                  ))}
-              </div>
-              <div>
-                <p>{new Date(sound.createdAt).toLocaleDateString()}</p>
-                <p>{sound.playCount} 回再生</p>
-              </div>
+        <Layout>
+          <Head>
+            <title>いいねした音声 / Prehnite</title>
+          </Head>
+          <h1 className="text-2xl font-black m-6">いいねした音声</h1>
+          {soundsList.length === 0 ? (
+            <h1 className="text-center">いいねした音声がありません</h1>
+          ) : (
+            soundsList.map((sound) => (
+              <Link href={`/play/${sound.id}`} key={sound.id}>
+                <div className="h-20 px-4 pt-2 border-b flex justify-between items-start border-neutral-700 hover:bg-neutral-700 transition">
+                  <div className="flex pt-2">
+                    <h1 className="font-bold mr-2">{sound.name}</h1>
+                    {sound.isMaleVoice !== null &&
+                      (sound.isMaleVoice ? (
+                        <Image
+                          alt="#"
+                          src={"/etc/male_icon.svg"}
+                          width={32}
+                          height={32}
+                        ></Image>
+                      ) : (
+                        <Image
+                          alt="#"
+                          src={"/etc/female_icon.svg"}
+                          width={32}
+                          height={32}
+                        ></Image>
+                      ))}
+                  </div>
+                  <div>
+                    <p>{new Date(sound.createdAt).toLocaleDateString()}</p>
+                    <p>{sound.playCount} 回再生</p>
+                  </div>
+                </div>
+              </Link>
+            ))
+          )}
+          <div className="h-28 pt-4 flex items-start justify-center">
+            <div className="flex items-center">
+              {currentPage > 1 && (
+                <button
+                  className="rounded-md px-2 py-2 border border-neutral-700 hover:bg-neutral-700 transition mr-4"
+                  onClick={() => {
+                    router.push(`/favorite?page=${currentPage - 1}`);
+                  }}
+                >
+                  <ArrowLongLeftIcon propClassName=""></ArrowLongLeftIcon>
+                </button>
+              )}
+              {`${currentPage} / ${totalPages}`}
+              {currentPage < totalPages && (
+                <button
+                  className="rounded-md px-2 py-2 border border-neutral-700 hover:bg-neutral-700 transition ml-4"
+                  onClick={() => {
+                    router.push(`/favorite?page=${currentPage + 1}`);
+                  }}
+                >
+                  <ArrowLongRightIcon propClassName=""></ArrowLongRightIcon>
+                </button>
+              )}
             </div>
-          </Link>
-        ))
+          </div>
+        </Layout>
       )}
-      <div className="h-28 pt-4 flex items-start justify-center">
-        <div className="flex items-center">
-          {currentPage > 1 && (
-            <button
-              className="rounded-md px-2 py-2 border border-neutral-700 hover:bg-neutral-700 transition mr-4"
-              onClick={() => {
-                router.push(`/favorite?page=${currentPage - 1}`);
-              }}
-            >
-              <ArrowLongLeftIcon propClassName=""></ArrowLongLeftIcon>
-            </button>
-          )}
-          {`${currentPage} / ${totalPages}`}
-          {currentPage < totalPages && (
-            <button
-              className="rounded-md px-2 py-2 border border-neutral-700 hover:bg-neutral-700 transition ml-4"
-              onClick={() => {
-                router.push(`/favorite?page=${currentPage + 1}`);
-              }}
-            >
-              <ArrowLongRightIcon propClassName=""></ArrowLongRightIcon>
-            </button>
-          )}
-        </div>
-      </div>
-    </Layout>
+    </>
   );
 };
 
