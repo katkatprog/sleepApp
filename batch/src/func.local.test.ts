ce5b4b29b779d7e24b670cc8b58b585b94135193
katkatprog <katkatprog@gmail.com>
2024-09-22 06:12:19 +0000

    github-actions上で実行できないテストをlocal用に切り出し

---
+++ batch/src/func.local.test.ts
@@ -0,0 +1,63 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { generateWordsList } from "./func";
+
+jest.mock("@google/generative-ai"); //モジュール全体をモック
+const MockGoogleGenerativeAI = GoogleGenerativeAI as jest.Mock; // TypeScriptでは型変換する必要がある
+
+describe("単語リスト生成処理のテスト(半角カンマ)", () => {
+  MockGoogleGenerativeAI.mockImplementationOnce(() => {
+    // geminiに代入するオブジェクトのモック
+    return {
+      getGenerativeModel: () => {
+        // modelに代入するオブジェクトのモック
+        return {
+          generateContent: async () => {
+            // resultに代入するオブジェクトのモック
+            return {
+              response: {
+                text: () => {
+                  return "dummy1, テーブル, 椅子, コンピュータ, dummy2";
+                },
+              },
+            };
+          },
+        };
+      },
+    };
+  });
+
+  it("正しくstring型配列を生成できることのテスト(半角カンマ)", async () => {
+    process.env.GEMINI_API_KEY = "dummy";
+    const result = await generateWordsList();
+    expect(result).toEqual(["てーぶる", "いす", "こんぴゅーた"]);
+  });
+});
+
+describe("単語リスト生成処理のテスト(全角カンマ)", () => {
+  MockGoogleGenerativeAI.mockImplementationOnce(() => {
+    // geminiに代入するオブジェクトのモック
+    return {
+      getGenerativeModel: () => {
+        // modelに代入するオブジェクトのモック
+        return {
+          generateContent: async () => {
+            // resultに代入するオブジェクトのモック
+            return {
+              response: {
+                text: () => {
+                  return "dummy1、テーブル、椅子、コンピュータ、dummy2";
+                },
+              },
+            };
+          },
+        };
+      },
+    };
+  });
+
+  it("正しくstring型配列を生成できることのテスト(全角カンマ)", async () => {
+    process.env.GEMINI_API_KEY = "dummy";
+    const result = await generateWordsList();
+    expect(result).toEqual(["てーぶる", "いす", "こんぴゅーた"]);
+  });
+});
--- batch/src/func.test.ts
+++ batch/src/func.test.ts
@@ -1,71 +1,10 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
 import {
   arrayShuffle,
-  generateWordsList,
   changeToCloudfrontUrl,
   wordsToSSML,
 } from "./func";
 
 jest.mock("@google/generative-ai"); //モジュール全体をモック
-const MockGoogleGenerativeAI = GoogleGenerativeAI as jest.Mock; // TypeScriptでは型変換する必要がある
-
-describe("単語リスト生成処理のテスト(半角カンマ)", () => {
-  MockGoogleGenerativeAI.mockImplementationOnce(() => {
-    // geminiに代入するオブジェクトのモック
-    return {
-      getGenerativeModel: () => {
-        // modelに代入するオブジェクトのモック
-        return {
-          generateContent: async () => {
-            // resultに代入するオブジェクトのモック
-            return {
-              response: {
-                text: () => {
-                  return "dummy1, テーブル, 椅子, コンピュータ, dummy2";
-                },
-              },
-            };
-          },
-        };
-      },
-    };
-  });
-
-  it("正しくstring型配列を生成できることのテスト(半角カンマ)", async () => {
-    process.env.GEMINI_API_KEY = "dummy";
-    const result = await generateWordsList();
-    expect(result).toEqual(["てーぶる", "いす", "こんぴゅーた"]);
-  });
-});
-
-describe("単語リスト生成処理のテスト(全角カンマ)", () => {
-  MockGoogleGenerativeAI.mockImplementationOnce(() => {
-    // geminiに代入するオブジェクトのモック
-    return {
-      getGenerativeModel: () => {
-        // modelに代入するオブジェクトのモック
-        return {
-          generateContent: async () => {
-            // resultに代入するオブジェクトのモック
-            return {
-              response: {
-                text: () => {
-                  return "dummy1、テーブル、椅子、コンピュータ、dummy2";
-                },
-              },
-            };
-          },
-        };
-      },
-    };
-  });
-
-  it("正しくstring型配列を生成できることのテスト(全角カンマ)", async () => {
-    process.env.GEMINI_API_KEY = "dummy";
-    const result = await generateWordsList();
-    expect(result).toEqual(["てーぶる", "いす", "こんぴゅーた"]);
-  });
-});
 
 describe("SSML作成処理のテスト", () => {
   it("文字列の配列から、期待したSSMLが作られるかテスト", () => {
