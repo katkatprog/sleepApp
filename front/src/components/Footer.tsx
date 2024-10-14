1bc5738ddcbea1832d2a20964684fdf3708312e1
katkatprog <katkatprog@gmail.com>
2024-10-14 21:47:04 +0900

    ユーザー情報取得完了後、フッター・サイドバーのアイコンを表示するよう修正

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -13,27 +13,31 @@ export const Footer = () => {
   return (
     <footer className="h-20 bg-neutral-800 border-gray-300 border-t fixed w-full left-0 bottom-0 flex justify-center lg:hidden">
       <div className="h-14 flex items-center justify-around max-w-xl w-full">
-        <Link href={"/search"}>
-          <SearchIcon propClassName="w-9 h-9 p-1 rounded-full stroke-2"></SearchIcon>
-        </Link>
-        {context.loginUser && (
+        {!context.isLoading && (
           <>
-            <Link href={"/request"}>
-              <PlusIcon prosClassName="w-9 h-9 p-1 rounded-full stroke-2"></PlusIcon>
+            <Link href={"/search"}>
+              <SearchIcon propClassName="w-9 h-9 p-1 rounded-full stroke-2"></SearchIcon>
             </Link>
-            <Link href={"/favorite"}>
-              <HeartIcon propClassName="w-9 h-9 p-1 rounded-full stroke-2"></HeartIcon>
-            </Link>
+            {context.loginUser && (
+              <>
+                <Link href={"/request"}>
+                  <PlusIcon prosClassName="w-9 h-9 p-1 rounded-full stroke-2"></PlusIcon>
+                </Link>
+                <Link href={"/favorite"}>
+                  <HeartIcon propClassName="w-9 h-9 p-1 rounded-full stroke-2"></HeartIcon>
+                </Link>
+              </>
+            )}
+            {context.loginUser ? (
+              // ログイン状態
+              <LogoutButton></LogoutButton>
+            ) : (
+              // 未ログイン状態
+              <Link href={"/login"}>
+                <LoginIcon propClassName="w-9 h-9 p-1 rounded-full stroke-2"></LoginIcon>
+              </Link>
+            )}
           </>
-        )}
-        {context.loginUser ? (
-          // ログイン状態
-          <LogoutButton></LogoutButton>
-        ) : (
-          // 未ログイン状態
-          <Link href={"/login"}>
-            <LoginIcon propClassName="w-9 h-9 p-1 rounded-full stroke-2"></LoginIcon>
-          </Link>
         )}
       </div>
     </footer>
