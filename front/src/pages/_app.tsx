eb11ab1d413b551bc8d4388b98d8726756bf4157
katkatprog <katkatprog@gmail.com>
2024-10-16 02:50:10 +0900

    cssインポート修正

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "./styles/globals.css";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import { User } from "@prisma/client";
