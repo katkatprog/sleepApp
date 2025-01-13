b330200eaf6bf6bb9a8d58d1e9027fab13dd6070
katkatprog <katkatprog@gmail.com>
2025-01-13 14:27:40 +0000

    loginUserの正常系追加

--- api/src/test/loginUser.test.ts
+++ api/src/test/loginUser.test.ts
@@ -1,10 +1,55 @@
 import request from "supertest";
 import { app } from "../app";
+import jwt from "jsonwebtoken";
+import { prismaMock } from "../prisma/singleton";
+import { User } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+// jwtをモック化する
+jest.mock("jsonwebtoken");
+const jwtMock = jest.mocked(jwt);
+
+// bcryptをモック化する
+jest.mock("bcrypt");
+const bcryptMock = jest.mocked(bcrypt);
 
 describe("Integration test (login-user)", () => {
   // prismaMockは各々のテスト前に初期化される(singleton.tsに設定あり)
 
+  beforeAll(() => {
+    // テスト用jwt設定
+    jwtMock.verify.mockImplementation(() => ({
+      userId: 1,
+    }));
+  });
+
   // ログインユーザー取得
+  test("[正常系]ログインユーザー取得", async () => {
+    // 事前準備
+    prismaMock.user.findUnique.mockResolvedValue({
+      id: 1,
+      name: "kat",
+      email: "katkatprog@example.com",
+      image: null,
+      hashedPassword: "dummyhashedPassword",
+    });
+
+    // 処理実行
+    const res = await request(app)
+      .get("/login-user")
+      .set("cookie", "token=dummytoken");
+
+    // 実行結果
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: 1,
+      name: "kat",
+      email: "katkatprog@example.com",
+      image: null,
+      hashedPassword: "dummyhashedPassword",
+    });
+  });
+
   test("[正常系]ログインユーザー取得(token無し)", async () => {
     // 処理実行
     const res = await request(app).get("/login-user");
@@ -15,21 +60,66 @@ describe("Integration test (login-user)", () => {
   });
 
   test("[正常系]ログインユーザー取得(token不正)", async () => {
+    // tokenが不正な場合、verifyでエラーになるためモックする
+    jwtMock.verify.mockImplementationOnce(() => {
+      throw new Error("");
+    });
+
     // 処理実行
-    // cookieには"dummy"というtokenを含める
-    const res = await request(app).get("/login-user").set("Cookie", "dummy");
+    const res = await request(app)
+      .get("/login-user")
+      .set("cookie", "token=invalidtoken");
 
     // 実行結果
     expect(res.status).toBe(200);
     expect(res.body).toBe(null);
   });
 
+  test("[正常系]ログインユーザー編集", async () => {
+    // 事前準備
+    prismaMock.user.findUnique.mockResolvedValue({
+      id: 1,
+      name: "kat",
+      email: "katkatprog@example.com",
+      image: null,
+      hashedPassword: "dummyhashedPassword",
+    });
+    prismaMock.user.update.mockResolvedValue({
+      id: 1,
+      name: "katupdate",
+      email: "katkatprog@example.com",
+      image: null,
+    } as User); // Userが期待されるが、実際にはhashedPasswordはカットするため、asを使用
+
+    // 処理実行
+    const res = await request(app)
+      .put("/login-user")
+      .set("cookie", "token=invalidtoken")
+      .send({
+        id: 1,
+        name: "katupdate",
+        email: "katkatprog@example.com",
+      });
+
+    // 実行結果
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: 1,
+      name: "katupdate",
+      email: "katkatprog@example.com",
+      image: null,
+    });
+  });
+
   test("[異常系]ログインユーザー編集(nameが空)", async () => {
     // 処理実行
-    const res = await request(app).put("/login-user").send({
-      name: "",
-      email: "test@example.com",
-    });
+    const res = await request(app)
+      .put("/login-user")
+      .set("cookie", "token=dummytoken")
+      .send({
+        name: "",
+        email: "test@example.com",
+      });
 
     // 実行結果
     expect(res.status).toBe(400);
@@ -38,10 +128,13 @@ describe("Integration test (login-user)", () => {
 
   test("[異常系]ログインユーザー編集(emailが空)", async () => {
     // 処理実行
-    const res = await request(app).put("/login-user").send({
-      name: "testuser",
-      email: "",
-    });
+    const res = await request(app)
+      .put("/login-user")
+      .set("cookie", "token=dummytoken")
+      .send({
+        name: "testuser",
+        email: "",
+      });
 
     // 実行結果
     expect(res.status).toBe(400);
@@ -52,10 +145,13 @@ describe("Integration test (login-user)", () => {
 
   test("[異常系]ログインユーザー編集(emailが不正)", async () => {
     // 処理実行
-    const res = await request(app).put("/login-user").send({
-      name: "testuser",
-      email: "test@",
-    });
+    const res = await request(app)
+      .put("/login-user")
+      .set("cookie", "token=dummytoken")
+      .send({
+        name: "testuser",
+        email: "test@",
+      });
 
     // 実行結果
     expect(res.status).toBe(400);
@@ -63,11 +159,16 @@ describe("Integration test (login-user)", () => {
   });
 
   test("[異常系]ログインユーザー編集(token不正)", async () => {
+    // tokenが不正な場合、verifyでエラーになるためモックする
+    jwtMock.verify.mockImplementationOnce(() => {
+      throw new Error("");
+    });
+
     // 処理実行
     // cookieには"dummy"というtokenを含める
     const res = await request(app)
       .put("/login-user")
-      .set("Cookie", "dummy")
+      .set("cookie", "token=invalidtoken")
       .send({
         id: 1,
         name: "testuser",
@@ -79,25 +180,57 @@ describe("Integration test (login-user)", () => {
     expect(res.text).toBe("認証情報が正しくありません。");
   });
 
-  test("[異常系]ログインユーザー編集(token不正)", async () => {
+  test("[異常系]ログインユーザー編集(ゲストユーザーのemailに変更することの防止)", async () => {
     // 処理実行
-    // cookieには"dummy"というtokenを含める
-    const res = await request(app).put("/login-user").send({
-      id: 1,
-      name: "guest",
-      email: "guest@example.com",
-    });
+    const res = await request(app)
+      .put("/login-user")
+      .set("cookie", "token=dummytoken")
+      .send({
+        id: 1,
+        name: "guest",
+        email: "guest@example.com",
+      });
 
     // 実行結果
     expect(res.status).toBe(400);
     expect(res.text).toBe("そのメールアドレスに変更することはできません。");
   });
 
+  test("[正常系]ログインユーザー削除", async () => {
+    // 事前準備
+    prismaMock.user.findUnique.mockResolvedValue({
+      id: 1,
+      name: "kat",
+      email: "katkatprog@example.com",
+      image: null,
+      hashedPassword: "dummyhashedPassword",
+    });
+    bcryptMock.compare.mockImplementationOnce(async () => true);
+
+    // 処理実行
+    const res = await request(app)
+      .delete("/login-user")
+      .set("cookie", "token=invalidtoken")
+      .send({
+        password: "P@ssw0rd",
+      });
+
+    // 実行結果
+    expect(res.status).toBe(200);
+    expect(res.text).toBe("OK");
+    // トークンがクリアされることのチェック
+    // headerのset-cookieは["token=; Path=/; ..."]という形で入っているため、下記のようなチェックをしている
+    expect(res.header["set-cookie"][0].includes("token=;")).toBe(true);
+  });
+
   test("[異常系]ログインユーザー削除(パスワードが空)", async () => {
     // 処理実行
-    const res = await request(app).delete("/login-user").send({
-      password: "",
-    });
+    const res = await request(app)
+      .delete("/login-user")
+      .set("cookie", "token=dummytoken")
+      .send({
+        password: "",
+      });
 
     // 実行結果
     expect(res.status).toBe(400);
@@ -105,13 +238,18 @@ describe("Integration test (login-user)", () => {
   });
 
   test("[異常系]ログインユーザー削除(tokenが不正)", async () => {
+    // tokenが不正な場合、verifyでエラーになるためモックする
+    jwtMock.verify.mockImplementationOnce(() => {
+      throw new Error("");
+    });
+
     // 処理実行
     const res = await request(app)
       .delete("/login-user")
+      .set("cookie", "token=invalidtoken")
       .send({
         password: "P@ssw0rd",
-      })
-      .set("Cookie", "dummy");
+      });
 
     // 実行結果
     expect(res.status).toBe(401);
